3a0e0714572462be3db729c22a8b7c21ce1870c9
Vladimir Samorodov <vsamorodov@akber-soft.ru>
2022-04-22 11:30:14 +0500

    Add comments for setting files path

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_command('source $HOME/.config/nvim/vim-plug/plugins.vim')
 vim.api.nvim_command('source $HOME/.config/nvim/general/settings.vim')
-require('general.settings')
-require('nvim-tree.settings')
+require('general.settings') -- $HOME/.config/nvim/lua/general/settings.lua
+require('nvim-tree.settings') -- $HOME/.config/nvim/lua/nvim-tree/settings.lua
 vim.api.nvim_command('source $HOME/.config/nvim/keys/mappings.vim')
 vim.api.nvim_command('source $HOME/.config/nvim/modules/coc.vim')
 vim.api.nvim_command('source $HOME/.config/nvim/modules/startify.vim')
-require('themes.settings') -- $HOME/.config/lua/themes/settings.lua
+require('themes.settings') -- $HOME/.config/nvim/lua/themes/settings.lua
