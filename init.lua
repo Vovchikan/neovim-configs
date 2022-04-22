edb296b446ddf28867b6e5056cf0f9f02254f1b5
Vladimir Samorodov <vsamorodov@akber-soft.ru>
2022-04-22 15:54:09 +0500

    Add settings.lua for which-key plugin

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.api.nvim_command('source $HOME/.config/nvim/general/settings.vim')
 require('general.settings') -- $HOME/.config/nvim/lua/general/settings.lua
 require('nvim-tree.settings') -- $HOME/.config/nvim/lua/nvim-tree/settings.lua
 require('lualine.settings') -- $HOME/.config/nvim/lua/lualine/settings.lua
+require('which-key.settings') -- $HOME/.config/nvim/lua/which-key/settings.lua
 vim.api.nvim_command('source $HOME/.config/nvim/keys/mappings.vim')
 vim.api.nvim_command('source $HOME/.config/nvim/modules/coc.vim')
 vim.api.nvim_command('source $HOME/.config/nvim/modules/startify.vim')
